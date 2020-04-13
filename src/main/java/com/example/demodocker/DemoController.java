2a3884f1593f84c7a4e9dacf0585a5a6d4e206b4
rohid dev <rohiddev@rohids-MacBook-Air.local>
2020-04-13 14:07:13 -0700

    m

--- src/main/java/com/example/demodocker/DemoController.java
+++ src/main/java/com/example/demodocker/DemoController.java
@@ -1,10 +1,34 @@
 package com.example.demodocker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+	
+	
+	@Value("${security.user.name}")
+    private String username; 
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Value("${security.user.password}")
+    private String password; 
 
 	@GetMapping("/hello")
 	public String greeting() {
@@ -13,6 +37,11 @@ public class DemoController {
 
 	@GetMapping("/")
 	public String greetingHello() {
+		
+		System.out.println("password" + getPassword());
+		System.out.println("username" + getUsername());
+		
+		
 		return "Hello World 2";
 	}
 
