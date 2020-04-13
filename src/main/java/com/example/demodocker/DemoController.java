e3095d5d354d50fb711a101c3e76d737a9e4681b
rohid dev <rohiddev@rohids-MacBook-Air.local>
2020-04-13 15:55:17 -0700

    kube secret

--- src/main/java/com/example/demodocker/DemoController.java
+++ src/main/java/com/example/demodocker/DemoController.java
@@ -1,12 +1,37 @@
 package com.example.demodocker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 	
+	
+	
+	@Value("${security.user.name}")
+    private String username; 
+	
+	public String getUsername() {
+		return username;
+	}
 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Value("${security.user.password}")
+    private String password; 
+    
+   
 
 	@GetMapping("/hello")
 	public String greeting() {
