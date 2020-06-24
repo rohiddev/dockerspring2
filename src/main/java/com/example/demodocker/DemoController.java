9307e9b4de94bed9b5a9e362216b1cc64dd78f2d
rohid dev <rohiddev@rohids-MacBook-Air.local>
2020-06-24 11:06:52 -0700

    u

--- src/main/java/com/example/demodocker/DemoController.java
+++ src/main/java/com/example/demodocker/DemoController.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter; 
+import java.io.IOException; 
+
 @RestController
 public class DemoController {
 	
-	
+	/*
 	
 	@Value("${security.user.name}")
     private String username; 
@@ -30,6 +35,8 @@ public class DemoController {
 
 	@Value("${security.user.password}")
     private String password; 
+	
+	*/
     
    
 
@@ -41,11 +48,57 @@ public class DemoController {
 	@GetMapping("/")
 	public String greetingHello() {
 		
-		System.out.println("password" + getPassword());
-	    System.out.println("username" + getUsername());
+		//System.out.println("password" + getPassword());
+	    //System.out.println("username" + getUsername());
+		
+		
 		
 		
 		return "Hello World";
+	}
+	
+	@GetMapping("/readwrite")
+	public String readwrite() throws IOException {
+	    String str = "File Handling in Java using "+ 
+                " FileWriter and FileReader"; 
+  
+        // attach a file to FileWriter  
+        FileWriter fw=new FileWriter("output.txt"); 
+  
+        // read character wise from string and write  
+        // into FileWriter  
+        for (int i = 0; i < str.length(); i++) 
+            fw.write(str.charAt(i)); 
+  
+        System.out.println("Writing successful"); 
+        //close the file  
+        fw.close(); 
+        
+        //
+        int ch; 
+        
+        // check if File exists or not 
+        FileReader fr=null; 
+        try
+        { 
+            fr = new FileReader("output.txt"); 
+        } 
+        catch (FileNotFoundException fe) 
+        { 
+            System.out.println("File not found"); 
+        } 
+  
+        // read from FileReader till the end of file 
+        while ((ch=fr.read())!=-1) 
+            System.out.print((char)ch); 
+  
+        // close the file 
+        fr.close(); 
+        
+        
+		
+		return "readwrite";
+		
 	}
 
 }
